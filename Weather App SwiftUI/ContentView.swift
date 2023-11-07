8e6c3d90b77a3853f17f92da7bc29913487bafe9
SevarJafarli <sevar.jafarli.std@bhos.edu.az>
2023-11-07 21:08:45 +0400

    Initial Commit

---
+++ Weather App SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Weather App SwiftUI
+//
+//  Created by Sevar Jafarli on 07.11.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Weather App SwiftUI/Weather_App_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_App_SwiftUIApp.swift
+//  Weather App SwiftUI
+//
+//  Created by Sevar Jafarli on 07.11.23.
+//
+
+import SwiftUI
+
+@main
+struct Weather_App_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
